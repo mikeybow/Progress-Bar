b5b20b3eb4c68eaf1079bda3f272ca13b0f79b20
Michael Bowman <micbowman25@gmail.com>
2021-02-07 16:47:46 -0600

    finished progress bar

---
+++ app.js
@@ -0,0 +1,49 @@
+const progress = document.querySelector("#progress");
+const circles = document.querySelectorAll(".circle");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+let currentActive = 1;
+
+const update = () => {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add("active")
+        } else {
+            circle.classList.remove("active")
+        }
+    })
+
+    const actives = document.querySelectorAll(".active");
+
+    progress.style.width = ((actives.length - 1) / (circles.length -1)) * 100 + "%";
+
+    if (currentActive === 1) {
+        prevBtn.disabled = true;
+    } else if (currentActive === circles.length) {
+        nextBtn.disabled = true;
+    } else {
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+    }
+};
+
+nextBtn.addEventListener("click", () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+});
